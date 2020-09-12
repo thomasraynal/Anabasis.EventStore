3ebd03a00c489b8f96040892dfa901ae066fd2c8
thomasraynal <me@thomasraynal.com>
2020-09-12 23:17:44 +0200

    angular integration

---
+++ src/js/main.js
@@ -0,0 +1,11 @@
+/**
+ * main.js
+ * http://www.codrops.com
+ *
+ * Licensed under the MIT license.
+ * http://www.opensource.org/licenses/mit-license.php
+ * 
+ * Copyright 2015, Codrops
+ * http://www.codrops.com
+ */
+
